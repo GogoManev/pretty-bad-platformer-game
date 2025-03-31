7663a1efae2d6f26a30feafa0481a09d0602231c
Gosho <georgy.manev@gmail.com>
2025-03-31 15:30:49 +0300

    Main menu and 1st part of tutorial

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    public static CameraControl instance;
     public Transform target;
     public GameObject Player;
-    public Vector3 offset = new(0, 0, -10);
+    public float beginningCoords;
+    public float endingCoords;
+    public Vector3 offset = new(0, 1f, -0.5f);
+    private void Awake()
+    {
+        instance = this;
+
+    }
+
     void Update()
     {
 
@@ -12,5 +21,16 @@ public class CameraControl : MonoBehaviour
     private void LateUpdate()
     {
         transform.position = target.position + offset;
+
+
+        if (transform.position.x < beginningCoords)
+        {
+            transform.position = new Vector3(beginningCoords, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.x > endingCoords)
+        {
+            transform.position = new Vector3(endingCoords, transform.position.y, transform.position.z);
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public bool canMove = true;
+    public bool canJump = true;
     public float speed = 5f;
+    public float jumpForce = 250f;
+    public Scene nextScene;
     private Rigidbody2D rb;
     void Start()
     {
@@ -15,21 +20,35 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         if (canMove)
         {
-            transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+            transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
         }
 
 
-        if (Input.GetKeyDown(KeyCode.Space) && canMove && transform.position.y < 17.20f)
+        if (Input.GetKeyDown(KeyCode.Space) && canMove && canJump)
         {
-            Debug.Log("JUMP");
-            rb.AddForce(new Vector2(rb.linearVelocity.x, 150));
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce));
         }
 
-        if (transform.position.x < -62.56f)
+        if(transform.position.x > CameraControl.instance.endingCoords + 10f)
         {
-            transform.position = new Vector3(-62.56f, transform.position.y, transform.position.z);
+            SceneManager.LoadScene(0);
         }
+    }
+    
+    void OnCollisionEnter2D(UnityEngine.Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = false;
+        }
+    }
 
 
+    void OnCollisionExit2D(UnityEngine.Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
     }
 }
