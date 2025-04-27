74c3da7bb21cbb076e6f7801d7aa8d329aa23d6c
Stefcho69 <stefan08vss@gmail.com>
2025-04-27 07:18:05 +0300

    .

--- Assets/Scripts/click.cs
+++ Assets/Scripts/click.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class click : MonoBehaviour
 {
-    // Update is called once per frame
+    // Update is called once per fram
+
+    public void Start()
+    {
+        Debug.Log("hello");
+    }
     void Update()
     {
         Debug.Log("click");
